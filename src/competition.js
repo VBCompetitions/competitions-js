4da14f2b98229b98fdde6461fe483b299a442768
Andy <andy@monkeysplayingpingpong.co.uk>
2024-06-22 17:44:35 +0100

    Implement club and team delete

--- src/competition.js
+++ src/competition.js
@@ -555,6 +555,13 @@ class Competition {
     }
 
     // Also remove team from any club's list
+    this.#clubs.forEach(club => {
+      club.deleteTeam(teamID)
+    })
+
+    // Then delete the team
+    delete this.#teamLookup[teamID]
+    this.#teams = this.#teams.filter(team => team.getID() !== teamID)
 
     return this
   }
@@ -731,7 +738,9 @@ class Competition {
     if (teamsInClub.length > 0) {
       throw new Error(`Club still contains teams with IDs: ${teamsInClub.map(t => `{${t.getID()}}`).join(', ')}`)
     }
-    // throw if any team still declares this club
+
+    delete this.#clubLookup[clubID]
+    this.#clubs = this.#clubs.filter(club => club.getID() !== clubID)
 
     return this
   }
